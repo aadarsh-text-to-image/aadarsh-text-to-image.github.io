7ab03b235143dbb74632e4566c91f1e3dbda8b8e
aadarsh-text-to-image <132989877+aadarsh-text-to-image@users.noreply.github.com>
2023-07-21 13:52:35 +0530

    Update API URL for Aa2I

---
+++ aa2i_v1.3/app.js
@@ -0,0 +1,49 @@
+// Aa2I v1.3
+
+function generateImage(e) {
+	e.preventDefault();
+	var inputText = document.getElementById("textInput").value;
+	var text = inputText.split(" ").join("%20");
+	var url = `https://aa2i.onrender.com/t2i/${text}`; // paste URL here
+	var imgelem = document.createElement("img");
+	imgelem.setAttribute("alt", "generated_image");
+	imgelem.setAttribute("id", "generatedImage");
+
+	let loadingElement = document.getElementById("loadingAnimation");
+	loadingElement.classList.add("play");
+
+	fetch(url)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			console.log(response);
+			return response.json();
+		})
+		.then((data) => {
+			if (document.querySelector(".result:has(#generatedImage)")) {
+				document
+					.querySelector(".result")
+					.removeChild(document.querySelector(".result").children[0]);
+			}
+			loadingElement.classList.remove("play");
+			document.querySelector(".result").appendChild(imgelem);
+			const generatedImage = document.getElementById("generatedImage");
+			generatedImage.src = data.url;
+		})
+		.catch((error) => {
+			loadingElement.classList.remove("play");
+			console.error("There was an error generating the image:", error);
+		});
+}
+
+let btn = document.getElementById("submitBtn");
+btn.addEventListener("click", generateImage);
+
+let input = document.getElementById("textInput");
+input.addEventListener("keypress", function (event) {
+	if (event.key === "Enter") {
+		event.preventDefault();
+		generateImage(event);
+	}
+});
